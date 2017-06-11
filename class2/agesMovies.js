8931291d2d5302983a30a54422ad8dc81e251175
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-11 12:03:24 -0600

    extra condition with else if, asignation of value inside parameters of function and reorganized of function callback

--- class2/agesMovies.js
+++ class2/agesMovies.js
@@ -27,10 +27,17 @@ const ageAlonso = 26
 
 const nameDante = 'Dante'
 const ageDante = 11
-
-function canWatchStarWars7(name, age){
+/*Si queremos reasignar un valor a cualquiera de estas constantes es un error en JS por lo tanto solamente elementos tipo var o let puede ser reasignados a valores nuevos */
+/*Si queremos que un parametro se evalue desde el inicio lo unico que tenemos que hacer es inicializarlo dentro de la funcion isWithAdult = false*/
+function canWatchStarWars7(name, age, isWithAdult = false){
     if(age >= pgStarWars7){
         alert(`${name} puede pasar a ver ${starWars7}`)
+    }else if(isWithAdult){ /*Este parametro por defecto regresa true o false depende de como se inicialize, si no se inicializa regresa false por defecto
+        aun sin enviar este parametro podemos ver que se puede invocar la funcion para obtener el resultado lo cual en esta condicion nos regresa un undefined,
+        si este valor no viene definido se evalua como false, y sigue evaluando la condicion */
+        alert(`${name} puede pasar a ver ${starWars7}.
+        Aunque su edad es de ${age}, se encuentra acompañada/o por un adulto`)
+
     }else{
         alert(`${name} no puede pasar a ver ${starWars7}.
         Tiene ${age} años y necesita tener ${pgStarWars7}`)
@@ -38,5 +45,5 @@ function canWatchStarWars7(name, age){
 }
 
 canWatchStarWars7(nameAlonso, ageAlonso);
-canWatchStarWars7(nameDante, ageDante);
+canWatchStarWars7(nameDante, ageDante, true);
 /*Esta es la manera de invocar las funciones fuera de su contexto con el fin de pasarle parametros */
