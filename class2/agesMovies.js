c6ca6c308fc20e39434ae1bea0b3c08f9c25612c
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-11 12:16:10 -0600

    Arrow function with canWatchStarWars7

--- class2/agesMovies.js
+++ class2/agesMovies.js
@@ -19,6 +19,36 @@ Los tipos de datos especiales son:
 Null Una variable que contiene null no contiene ningún número, cadena, valor booleano, matriz u objeto.  Para borrar el contenido de una variable (sin eliminar la variable), asígnele el valor null.  
 No definido undefined Se devuelve el valor undefined cuando se usa una propiedad de objeto que no existe o una variable que se ha declarado, pero nunca ha tenido un valor asignado.*/
 
+/*const starWars7 = 'StarWars: El despertar de la Fuerza'
+let pgStarWars7 = 13
+
+const nameAlonso = 'Alonso'
+let ageAlonso = 26
+
+const nameDante = 'Dante'
+let ageDante = 11
+/*Si queremos reasignar un valor a cualquiera de estas constantes es un error en JS por lo tanto solamente elementos tipo var o let puede ser reasignados a valores nuevos */
+/*Si queremos que un parametro se evalue desde el inicio lo unico que tenemos que hacer es inicializarlo dentro de la funcion isWithAdult = false*/
+/*function canWatchStarWars7(name, age, isWithAdult = false){
+    if(age >= pgStarWars7){
+        alert(`${name} puede pasar a ver ${starWars7}`)
+    }else if(isWithAdult){ /*Este parametro por defecto regresa true o false depende de como se inicialize, si no se inicializa regresa false por defecto
+        aun sin enviar este parametro podemos ver que se puede invocar la funcion para obtener el resultado lo cual en esta condicion nos regresa un undefined,
+        si este valor no viene definido se evalua como false, y sigue evaluando la condicion */
+        /*alert(`${name} puede pasar a ver ${starWars7}.
+        Aunque su edad es de ${age}, se encuentra acompañada/o por un adulto`)
+
+    }else{
+        alert(`${name} no puede pasar a ver ${starWars7}.
+        Tiene ${age} años y necesita tener ${pgStarWars7}`)
+    }
+}
+
+canWatchStarWars7(nameAlonso, ageAlonso);
+canWatchStarWars7(nameDante, ageDante, true); */
+/*Esta es la manera de invocar las funciones fuera de su contexto con el fin de pasarle parametros */
+
+/*Ejercicio Reescribir esta funcion con arrow functions como en la clase anterior */
 const starWars7 = 'StarWars: El despertar de la Fuerza'
 let pgStarWars7 = 13
 
@@ -29,21 +59,11 @@ const nameDante = 'Dante'
 let ageDante = 11
 /*Si queremos reasignar un valor a cualquiera de estas constantes es un error en JS por lo tanto solamente elementos tipo var o let puede ser reasignados a valores nuevos */
 /*Si queremos que un parametro se evalue desde el inicio lo unico que tenemos que hacer es inicializarlo dentro de la funcion isWithAdult = false*/
-function canWatchStarWars7(name, age, isWithAdult = false){
-    if(age >= pgStarWars7){
-        alert(`${name} puede pasar a ver ${starWars7}`)
-    }else if(isWithAdult){ /*Este parametro por defecto regresa true o false depende de como se inicialize, si no se inicializa regresa false por defecto
-        aun sin enviar este parametro podemos ver que se puede invocar la funcion para obtener el resultado lo cual en esta condicion nos regresa un undefined,
-        si este valor no viene definido se evalua como false, y sigue evaluando la condicion */
-        alert(`${name} puede pasar a ver ${starWars7}.
-        Aunque su edad es de ${age}, se encuentra acompañada/o por un adulto`)
-
-    }else{
-        alert(`${name} no puede pasar a ver ${starWars7}.
-        Tiene ${age} años y necesita tener ${pgStarWars7}`)
-    }
+const canWatchStarWars7 =  (name, age, isWithAdult = false)=>{
+    if(age >= pgStarWars7) {alert(`${name} puede pasar a ver ${starWars7}`)}
+    else if(isWithAdult){ alert(`${name} puede pasar a ver ${starWars7}. Aunque su edad es de ${age}, se encuentra acompañada/o por un adulto`)}
+    else{ alert(`${name} no puede pasar a ver ${starWars7}. Tiene ${age} años y necesita tener ${pgStarWars7}`)}
 }
 
 canWatchStarWars7(nameAlonso, ageAlonso);
 canWatchStarWars7(nameDante, ageDante, true);
-/*Esta es la manera de invocar las funciones fuera de su contexto con el fin de pasarle parametros */
