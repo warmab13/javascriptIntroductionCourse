09fdfa652ebe92db0ad9225788b3ee06fb9231cc
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-11 03:30:52 -0600

    Acento invertido template

---
+++ class1/triangulo.js
@@ -0,0 +1,6 @@
+//El codigo funciona pero el problema es que no se ve muy bien, no es nada descriptivo, debemos de refactorizar este codigo 
+//console.log('El area de un triagulo de base 5 y altura 7 es: ' + 5 * 7 / 2);
+//Javascript permite utilizar comillas "" o '' para realizar cadenas de caracteres, sin embargo tambien podemos omitir el signo de mas para concatenar
+console.log(`El area de un triagulo de base 5 y altura 7 es:  ${ 5 * 7 / 2}`);
+//Utilizando el acento invertido podemos declarar un template que renderize el codigo y lo interprete en el navegador.
+//Ahora el fin es generar una funcion para que los parametros del triangulo sean variables,
