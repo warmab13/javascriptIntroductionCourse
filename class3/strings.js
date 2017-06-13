b1fe6f1fa36d3ab16f24647ec89ef29f561440a5
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-13 00:15:28 -0600

    second rule of language is correct

--- class3/strings.js
+++ class3/strings.js
@@ -21,8 +21,18 @@ function pzoom(str){
         //slice nos permite cortar caracteres de una cade
         translation = str.slice(0, -2); //Desde el final sacar dos caracteres y hasta el inicio
     }
+
+    //Si la palabra inicia con Z, se le añade "pe" al final
+    //startsWith nos permite preguntar si una palabra inicia con alguna letra
+    if(str.toLowerCase().startsWith('z')){
+        //Esto se puede reescribir con un +=
+        //translation = translation + "pe"
+        translation += 'pe'
+    }
     return translation;
 }
 
 
-console.log(pzoom("Programar"));
+console.log(pzoom("Programar")); //Program
+console.log(pzoom("Zorro")); //Zorrope
+console.log(pzoom("Zarpar")); //Zarppe
