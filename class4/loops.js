fb65c412d8cc1e89f8832396927820bc258d3224
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-13 00:53:30 -0600

    kms run in the week

--- class4/loops.js
+++ class4/loops.js
@@ -11,12 +11,23 @@ const days = [
     'domingo'
 ]
 
-//Vamos a escribir una funcion para decir cuantos km corre una persona en un dia
+//Vamos a escribir una funcion para decir cuantos km corre una persona en un dia,
+//Este valor va a ser un valor random entre 5 y 15 km
+function correr(){
+    //Esta funcion un regresa un valor entre 0 y 1
+    const min = 5
+    const max = 15
+    //Math.floor Devuelve el máximo entero menor o igual a un número.
+    return Math.floor(Math.random() * (max-min)) + min
 
+}
 //El ciclo for esta compuesto de una variable de conteo casi siempre de utiliza i como indice,
 // una condicion para finalizar el ciclo,
 // y la variable i que funciona como indice es la que va a ir aumentando o disminuyendo
 // cuando se requiera
+let totalKms = 0
 for(let i = 0; i < days.length; i++){
-    console.log(`El día ${i+1} es el ${days[i]}`)
+    const kms = correr()
+    totalKms += kms
+    console.log(`El día ${days[i]} ${nombre} corrió ${kms} kms`)
 }
