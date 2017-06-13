84b13c92dd35a159e978c88474a45948b5f4159a
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-13 01:18:39 -0600

    finish algorith for epic battle

--- class5/loopswhile.js
+++ class5/loopswhile.js
@@ -10,12 +10,37 @@ let vidaSuperman = 100
 const MIN_POWER = 5
 const MAX_POWER = 12
 
-function ambosSiguenVivos(){
+/*function ambosSiguenVivos(){
    return vidaGoku > 0 && vidaSuperman > 0 
-}
+}*/
 
-let round = 1
+const ambosSiguenVivos = () => vidaGoku > 0 && vidaSuperman > 0 
+const calcularGolpe = () => Math.round(Math.random() * (MAX_POWER - MIN_POWER) + MIN_POWER)
+const gokuSigueVivo = () => vidaGoku > 0
+
+let round = 0
 //Mientras se cumpla una condicion este bloque se sigue ejecutando
 while(ambosSiguenVivos()){
+    round++
+    console.log(`Round ${round}`)
 
+    const golpeGoku = calcularGolpe()
+    const golpeSuperman = calcularGolpe()
+
+    if(golpeGoku > golpeSuperman){
+        //Ataca Goku
+        console.log(`Goku ataca a Superman  con un golpe de ${golpeGoku}`)
+        vidaSuperman -= golpeGoku
+        console.log(`Superman queda en ${vidaSuperman} de vida`)
+    }else{
+        //Ataca Superman
+        console.log(`Superman ataca a Goku  con un golpe de ${golpeSuperman}`)
+        vidaGoku -= golpeSuperman
+        console.log(`Goku queda en ${vidaGoku} de vida`)
+    }
+}
+if(gokuSigueVivo()){
+    console.log(`Goku ganó la pelea. Su vida es de: ${vidaGoku}`)
+}else{
+    console.log(`Superman ganó la pelea. Su vida es de: ${vidaSuperman}`)
 }
