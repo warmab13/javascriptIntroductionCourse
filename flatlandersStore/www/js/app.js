db17429d9689054c4498e190baffb54fba5d2e4e
Alonso Salcido <alonsosalcido13@gmail.com>
2017-06-15 10:58:59 -0600

    ReviewController form for new review

--- flatlandersStore/www/js/app.js
+++ flatlandersStore/www/js/app.js
@@ -125,4 +125,13 @@
         }
     });
 
+    app.controller('ReviewController', function(){
+        this.review = {};
+
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        }
+    });
+
 })();
